8c7f38967848f8a32f0b3eef5bd1340e2ae0cc41
shuoO-24 <lishuo2332@gmail.com>
2021-07-30 22:48:19 -0700

    Add files

---
+++ tests/part-b/custom/inputs/imm.s
@@ -0,0 +1,22 @@
+slli t1 s0 5
+slti t1 x0 10
+slti t1 x0 -10
+xori t1 s0 20
+srli t1 s0 19
+srai t1 s0 18
+ori t1 s0 0xf0
+andi t1 s0 0xa0
+auipc ra 12
+lui s0 18
+addi s0 s0 95
+add s0 x0 s0
+addi t0 x0 10
+sll t1 s0 t0
+slti t1 x0 20
+slti t1 x0 31
+slli t1 s0 30
+ori t1 s0 0xf0
+xori t2 s0 31
+srli t2 s0 20
+srai t2 s0 19
+andi t2 s0 0xa0
---
+++ tests/part-b/custom/inputs/mem.s
@@ -0,0 +1,14 @@
+sh s0 99(x0)
+lh ra 99(x0)
+lw ra 99(x0)
+addi s0 x0 0x99
+lh ra 109(x0)
+lw ra 108(x0)
+sh s0 104(x0)
+lh ra 110(x0)
+lw ra 112(x0)
+sb s0 118(x0)
+lw ra 116(x0)
+sb s0 121(x0)
+auipc ra 1231
+lui s0 74565
