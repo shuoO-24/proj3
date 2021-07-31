eb0ba4f2ea84704e9b1a9565ab7852cf17d714ee
shuoO-24 <lishuo2332@gmail.com>
2021-07-30 23:54:04 -0700

    test update

--- tests/part-b/custom/inputs/branch.s
+++ tests/part-b/custom/inputs/branch.s
@@ -1,32 +1,22 @@
-addi t0 x0 -10
-addi t1 x0 10
-
-b0:
-    bne s0 x0 b1
-
-b1:
-    bne s0 s0 b0
-    beq s0 s0 b2
-
-b2:
-    blt x0 t0 b1
-    bltu x0 t0 b3
-b:
+addi t0 x0 -1
+addi t1 x0 1
+p0:
+    bne s0 x0 p1
+p1:
+    bne s0 s0 p0
+    beq s0 s0 p2
+p2:
+    blt x0 t0 p3
+    bltu x0 t0 p0
+p3:
+    bge x0 t1 p4
+    bgeu t0 x0 p1
+p4:
+    jal ra p5
+p41:
+    jal x0 end
+p5:
     jalr ra ra 0
-
-b3:
-    bge x0 t1 b4
-    bgeu t0 x0 b2
-
-b4:
-    jal ra b5
-
-b5:
-    jal x0 exit
-
-b7:
-    jalr ra ra 0
-
-exit:
+end:
     add x0 x0 x0
     
